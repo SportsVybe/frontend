d9bb140ecda9fb67e4158e675f299c37dc23ef3d
Avrahm Kleinholz <me@avrahm.com>
2020-03-01 21:55:18 -0500

    testing user logged in

--- src/services/base.js
+++ src/services/base.js
@@ -2,7 +2,7 @@ let baseURL;
 
 process.env.NODE_ENV === "production"
   ? //? (baseURL = 'here should be your production endpoint')
-    (baseURL = "https://sportsvybe.herokuapp.com")
+    (baseURL = "https://sportsvybe-backend.herokuapp.com")
   : (baseURL = "http://localhost:5000");
 
   export default baseURL
